7ab766e9fd9f6b648175a36c86a05f0f75cae4fd
RajrupDasid <rajrupofficialid@protonmail.com>
2023-07-08 01:42:49 +0530

    token creation logic rectified
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- server/users/auth.py
+++ server/users/auth.py
@@ -3,23 +3,33 @@ from jose import jwt,JWTError, ExpiredSignatureError
 from typing import Annotated
 from server import config
 from .models import User
+from .security import verify_hash
 
 settings = config.get_settings()
 
-def authenticate(email,password):
+def authenticate(email, password):
+    # step 1
     try:
-        user_obj =  User.objects.get(email=email)
+        #q = User.objects.filter(email=)
+        q = User.objects.filter(email=email).allow_filtering()
+        user_obj = q.get()
+        
+
     except Exception as e:
+        print(e)
         user_obj = None
-    if not user_obj.verify_password(password):
-        return None
+    if user_obj:
+        if not user_obj.verify_password(password):
+            return None
     return user_obj
+
+
 
 def login(user_obj,expires=settings.session_duration):
     raw_data = {
         "uid":f"{user_obj.uid}",
         "role":"mainuser",
-        "exp": datetme.datetime.utcnow() + datetime.timedelta(seconds=expires)
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=expires)
     }
     return jwt.encode(raw_data, settings.secret_key, algorithm=settings.jwt_algorithm)
 
